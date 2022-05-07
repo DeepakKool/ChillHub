a5e12ae193f5923460374838a084501f1dc64533
Deepak Karnadhar <karandhardeepak94222@gmail.com>
2022-05-07 20:32:58 +0530

    ReactApp

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useEffect , useState } from "react";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard.jsx"
+const API_URL = 'http://omdbapi.com?apikey=c032e2d7'
+const App = () => {
+    const [ movies , setMovies ] = useState([]);
+    const [ searchTerm , setSearchTerm ] = useState('');
+    const searchMovies = async(title) => {
+    const response = await fetch( `${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);     
+    }
+    useEffect(() => {
+        searchMovies(''); }
+    , [] )
+    return (
+    <div className="app">
+        <h1>ChillHub</h1>
+    <div className="search">    
+        <input placeholder="Search for movies & games" 
+        value= {searchTerm}
+        onChange={ (e) => setSearchTerm(e.target.value)
+        }
+        />
+        <img src={SearchIcon}
+        alt="search" 
+        onClick={ () => searchMovies(searchTerm) }
+        />
+    </div>
+    {
+    movies?.length > 0 ? (
+    <div className="container">
+        {movies.map((movie) => (<MovieCard movie={movie} />))}    
+    </div> ) : (
+        <div className="empty">
+        <h2>Search for Awesome Movies & Games</h2>    
+        </div>)}
+    </div>
+    );
+}
+export default App;
